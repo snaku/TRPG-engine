66797eb44470c85bc26ebafb05257bca4ac2ceef
snaku <harischali543@gmail.com>
2025-06-17 10:40:28 +0200

    validation layers

--- Include/VulkanInstance.hpp
+++ Include/VulkanInstance.hpp
@@ -10,6 +10,7 @@ public:
 
 private:
 	void createInstance();
+	bool checkValidationLayer();
 
 	VulkanContext& vkCtx_;
 };
--- src/VulkanInstance.cpp
+++ src/VulkanInstance.cpp
@@ -35,7 +35,43 @@ void VulkanInstance::createInstance()
 	createInfo.pApplicationInfo = &appInfo;
 	createInfo.enabledExtensionCount = glfwExtCount;
 	createInfo.ppEnabledExtensionNames = glfwExt;
-	createInfo.enabledLayerCount = 0; // TODO
+
+#ifdef _DEBUG
+	vkCtx_.enableValidationLayers = true;
+	vkCtx_.validationLayers.push_back("VK_LAYER_KHRONOS_validation");
+#else
+	vkCtx_.enableValidationLayers = false;
+#endif
+
+	if (vkCtx_.enableValidationLayers && checkValidationLayer())
+	{
+		createInfo.enabledLayerCount = (uint32_t)vkCtx_.validationLayers.size();
+		createInfo.ppEnabledLayerNames = vkCtx_.validationLayers.data();
+		std::cout << "validation layer !" << std::endl;
+	}
+	else
+	{
+		createInfo.enabledLayerCount = 0;
+		createInfo.ppEnabledLayerNames = nullptr;
+	}
 
 	VK_CHECK(vkCreateInstance(&createInfo, nullptr, &vkCtx_.instance));
+}
+
+bool VulkanInstance::checkValidationLayer()
+{
+	uint32_t layerCount = 0;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> layerProperties(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, layerProperties.data());
+
+	for (const auto& lp : layerProperties)
+	{
+		if (strcmp(lp.layerName, "VK_LAYER_KHRONOS_validation") == 0)
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
