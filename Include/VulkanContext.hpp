7be025e17a05dbae35c09b1c2d5bedcc46d2907b
snaku <harischali543@gmail.com>
2025-07-01 01:25:29 +0200

    added a lot of handles (and there's things that shouldn't be there)

--- Include/VulkanContext.hpp
+++ Include/VulkanContext.hpp
@@ -22,17 +22,33 @@ struct VulkanContext
 	VkQueue graphicsQueue = VK_NULL_HANDLE;
 	VkQueue presentQueue = VK_NULL_HANDLE;
 
+	// swapchain
 	VkSwapchainKHR swapchain = VK_NULL_HANDLE;
 	std::vector<VkImage> swapchainImages;
 	std::vector<VkImageView> swapchainImageViews;
-
-	VkCommandPool commandPool = VK_NULL_HANDLE;
 
 	VkRenderPass renderPass = VK_NULL_HANDLE;
 
 	// pipeline
 	VkPipeline pipeline = VK_NULL_HANDLE;
 	VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
+
+	std::vector<VkFramebuffer> swapChainFrameBuffers;
+
+	VkCommandPool commandPool = VK_NULL_HANDLE;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	// semaphores
+	VkSemaphore imgAvailableSemaphore = VK_NULL_HANDLE;
+	VkSemaphore renderFinishedSemaphore = VK_NULL_HANDLE;
+
+	VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
+	VkDescriptorSet descriptorSet;
+
+	VkBuffer vertexBuffer; // temporary
+	VkBuffer uniformBuffer; // temporary
+	VkDeviceMemory vertexBufferMemory;
+	VkDeviceMemory uniformBufferMemory;
 
 	// validation layers
 	std::vector<const char*> validationLayers;
