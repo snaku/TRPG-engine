81a709b51d59177fe2a47a553e38c4274c4e889f
snaku <harischali543@gmail.com>
2025-07-22 02:09:30 +0200

    loadTexture virtual method + const ref to Camera in renderFrame

--- Include/Renderer.hpp
+++ Include/Renderer.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
+
+class Camera;
 
 class Renderer
 {
@@ -8,8 +11,9 @@ public:
     virtual ~Renderer() = default;
 
     virtual void init() = 0;
-    virtual void createPipeline(const std::string& vertexShader, const std::string& fragmentShader) = 0;
+    virtual void createPipeline(std::string_view vertexShader, std::string_view fragmentShader) = 0;
     virtual void prepareRender() = 0;
     virtual void createObject() = 0;
-    virtual void renderFrame() = 0;
+    virtual void renderFrame(const Camera& camera) = 0;
+    virtual void loadTexture(const std::filesystem::path& texturePath) = 0;
 };
