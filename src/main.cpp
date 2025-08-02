6d455ca56c7d9ad070039fb8227c3685308209e6
snaku <harischali543@gmail.com>
2025-08-02 23:35:43 +0200

    test input + GameObject and Scene class

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,74 @@
 #include <GLFW/glfw3.h>
 #include "Include/Engine.hpp"
+#include "Include/GameObject.hpp"
+#include "Include/Scene.hpp"
+#include "Include/TransformComponents.hpp"
 
 #include <iostream>
+
+void processInput(Camera& camera, float deltaTime)
+{
+	float cameraMovementSpeed = 7.5f;
+	float cameraRotationSpeed = 50.0f;
+
+	if (engine::shiftKey() == GLFW_PRESS)
+	{
+		cameraMovementSpeed *= 2.0f;
+		cameraRotationSpeed *= 2.0f;
+	}
+
+	// ZQSD keys
+	if (engine::up() == GLFW_PRESS)
+		camera.moveForward(cameraMovementSpeed * deltaTime);
+	
+	if (engine::down() == GLFW_PRESS)
+		camera.moveForward(-cameraMovementSpeed * deltaTime);
+	
+	if (engine::left() == GLFW_PRESS)
+		camera.moveRight(-cameraMovementSpeed * deltaTime);
+	
+	if (engine::right() == GLFW_PRESS)
+		camera.moveRight(cameraMovementSpeed * deltaTime);
+	
+	// arrow keys
+	if (engine::upArrow() == GLFW_PRESS) 
+		camera.rotateX(cameraRotationSpeed * deltaTime);
+
+	if (engine::downArrow() == GLFW_PRESS)
+		camera.rotateX(-cameraRotationSpeed * deltaTime);
+
+	if (engine::leftArrow() == GLFW_PRESS)
+		camera.rotateY(-cameraRotationSpeed * deltaTime);
+	
+	if (engine::rightArrow() == GLFW_PRESS)
+		camera.rotateY(cameraRotationSpeed * deltaTime);		
+}
 
 int main()
 {
 	if (!engine::initWindow(800, 600, "Vulkan")) 
 		return -1;
 	
+	Camera camera({0.0f, 10.0f, 10.0f}, -55.0f, -35.0f, 45.0f);
+	engine::Scene scene;
+
+	auto& object = scene.createGameObject("object");
+	auto* transformComp = object.addComponent<TransformComponent>();
+
 	try 
 	{
 		engine::initEngine();
 
+		for (const auto& renderable : scene.getRenderableData())
+		{
+			engine::createMesh(renderable);
+		}
+
 		engine::loadTexture("testAssets/takuji.jpg");
-		Camera camera({0.0f, 2.0f, 5.0f}, -50.0f, 0.0f, 90.0f);
-		engine::createObject();
 		
 		engine::createPipeline("shaders/vert.spv", "shaders/frag.spv");
 		engine::prepareRender();
 		
-		float speed = 5.0f;
 		float lastTime = 0.0f;
 
 		while (!engine::windowShouldClose())
@@ -30,24 +79,13 @@ int main()
 			float deltaTime = currentTime - lastTime;
 			lastTime = currentTime;
 
-			if (engine::up() == GLFW_PRESS)
-			{
-				camera.moveForward(speed * deltaTime);
-			}
-			if (engine::down() == GLFW_PRESS)
-			{
-				camera.moveForward(-speed * deltaTime);
-			}
-			if (engine::left() == GLFW_PRESS)
-			{
-				camera.moveRight(-speed * deltaTime);
-			}
-			if (engine::right() == GLFW_PRESS)
-			{
-				camera.moveRight(speed * deltaTime);
-			}
+			processInput(camera, deltaTime);
 
-			engine::render(camera);
+			transformComp->scale += 0.1f * deltaTime;
+
+			scene.update(deltaTime);
+
+			engine::render(scene, camera);
 		}
 	}
 	catch (const std::exception& e)
