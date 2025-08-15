94523a5266c216ae21d3ebd86d5e75de5add84c2
snaku <harischali543@gmail.com>
2025-08-15 11:52:13 +0200

    create ShapeType enum

---
+++ Include/ShapeType.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+enum class ShapeType
+{
+    NONE = 0,
+
+    // 2D shapes
+    TRIANGLE_2D,
+    QUAD_2D,
+    PARALLELOGRAM_2D,
+    CIRCLE_2D,
+    LINE_2D,
+
+    // 3D shapes
+    CUBE_3D,
+    SPHERE_3D
+};
