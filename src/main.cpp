313af35821b7c439062ac3e92b99a4f04ad5c36d
snaku <harischali543@gmail.com>
2025-08-15 12:08:20 +0200

    testing the engine (parallelogram billboard + a cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <GLFW/glfw3.h>
+#include <glm/gtc/quaternion.hpp>
+
 #include "Include/Engine.hpp"
 #include "Include/GameObject.hpp"
+#include "Include/ShapeType.hpp"
 #include "Include/Scene.hpp"
 #include "Include/TransformComponents.hpp"
+#include "Include/JumpComponent.hpp"
 
 #include <iostream>
+#include <chrono>
+
+/*const int gridSize = 20; // 20 x 20 x 20 = 8000 objects
+	const float spacing = 2.0f;
+	float xSpacing = 1.0;
+
+	for (int x = 0; x < gridSize; ++x)
+	{
+		for (int y = 0; y < gridSize; ++y)
+		{
+			for (int z = 0; z < gridSize; ++z)
+			{
+				// std::string name = "obj_" + std::to_string(x) + "_" + std::to_string(y) + "_" + std::to_string(z);
+				engine::GameObject& obj = scene.createGameObject("object");
+				TransformComponent* transform = obj.addComponent<TransformComponent>();
+				transform->position = { x + xSpacing, y + xSpacing, z * spacing };
+			}
+		}
+	}*/
 
 void processInput(Camera& camera, float deltaTime)
 {
 	float cameraMovementSpeed = 7.5f;
-	float cameraRotationSpeed = 50.0f;
+	float cameraRotationSpeed = 70.0f;
 
 	if (engine::shiftKey() == GLFW_PRESS)
 	{
@@ -46,30 +69,43 @@ void processInput(Camera& camera, float deltaTime)
 
 int main()
 {
-	if (!engine::initWindow(800, 600, "Vulkan")) 
+	if (!engine::initWindow(1920, 1080, "Vulkan")) 
 		return -1;
 	
 	Camera camera({0.0f, 10.0f, 10.0f}, -55.0f, -35.0f, 45.0f);
+
 	engine::Scene scene;
 
-	auto& object = scene.createGameObject("object");
-	auto* transformComp = object.addComponent<TransformComponent>();
+	engine::GameObject& cube = scene.createGameObject("cube", ShapeType::CUBE_3D);
+	TransformComponent* transformCube = cube.addComponent<TransformComponent>();
+
+	engine::GameObject& parallelogram = scene.createGameObject("parallelogram", ShapeType::PARALLELOGRAM_2D);
+	TransformComponent* transformParallelogram = parallelogram.addComponent<TransformComponent>();
 
 	try 
 	{
 		engine::initEngine();
+
+		auto start = std::chrono::high_resolution_clock::now();
 
 		for (const auto& renderable : scene.getRenderableData())
 		{
 			engine::createMesh(renderable);
 		}
 
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> duration = end - start;
+
+		std::cout << "Creating " << scene.getGameObjects().size() << " GameObjects took " << duration.count() << " secondes" << std::endl;
+
 		engine::loadTexture("testAssets/takuji.jpg");
 		
 		engine::createPipeline("shaders/vert.spv", "shaders/frag.spv");
 		engine::prepareRender();
-		
+
 		float lastTime = 0.0f;
+		float fpsLastTime = lastTime;
+		int nbFrames = 0;
 
 		while (!engine::windowShouldClose())
 		{
@@ -79,9 +115,29 @@ int main()
 			float deltaTime = currentTime - lastTime;
 			lastTime = currentTime;
 
+			nbFrames++;
+
+			if (currentTime - fpsLastTime >= 1.0f)
+			{
+				std::cout << "FPS: " << nbFrames << std::endl;
+				nbFrames = 0;
+				fpsLastTime = currentTime;
+			}
+
 			processInput(camera, deltaTime);
 
-			transformComp->scale += 0.1f * deltaTime;
+			glm::vec3 dir = camera.getPosition() - transformParallelogram->position;
+			dir.y = 0.0f;
+			dir = glm::normalize(dir);
+
+			float angleY = atan2(dir.x, dir.z); 
+			float angleYDegrees = glm::degrees(angleY);
+			float correctedAngleYDegrees = angleYDegrees + 180.0f;
+			if (correctedAngleYDegrees > 360.0f)
+				correctedAngleYDegrees -= 360.0f;
+
+			transformParallelogram->rotation = glm::vec3(0.0f, correctedAngleYDegrees, 0.0f);
+			transformCube->rotation.y += 30.0f * deltaTime;
 
 			scene.update(deltaTime);
 
